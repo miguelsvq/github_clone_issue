3f086cb4b240dde6c8b7c7cee4ab8ff6e9248752
mpereram <51081039+mpereram@users.noreply.github.com>
2024-04-20 12:59:23 +0200

    Clone to other repos added

--- content.js
+++ content.js
@@ -6,16 +6,23 @@ let extOptions = {
     titlePrefix: TITLE_PREFIX,
     buttonsInList: false,
     buttonInIssuePage: true,
-    newWithTemplate: false
+    newWithTemplate: false,
+    otherRepos: []
 };
 
 // Initialize GitHub token
 let gitToken = '';
-
+let currentRepo = '';
 // Fetch extension options from storage
 chrome.storage.local.get(['extOptions', 'gitToken'], ({ extOptions: storedExtOptions, gitToken: storedGitToken }) => {
     extOptions = storedExtOptions || extOptions;
     gitToken = storedGitToken || gitToken;
+    console.log(extOptions);
+    const url = new URL(window.location.href);
+    const parts = url.pathname.split('/');
+    if(parts.length>2){
+        currentRepo=parts[1]+'/'+parts[2];
+    }
     // Initialize extension
     init();
 });
@@ -25,6 +32,14 @@ const init = () => {
     addButton();
     addCloneButtonToIssueLinks();
     addNewFromTemplateButton();
+    document.body.addEventListener("click", function (evt) {
+        if(evt.target.classList.contains('ext_trigger')){
+            evt.target.classList.add('me');
+        }
+        const collection = document.querySelectorAll('.ext_trigger:not(.me)');
+        collection.forEach(item => item.classList.remove('open'));
+        evt.target.classList.remove('me');
+    });
 };
 
 // check issues list page or issue
@@ -55,13 +70,50 @@ const addButton = () => {
 };
 
 // Create "Clone" button element
-const createCloneButton = () => {
+const createCloneButton = (issueLinkHref) => {
+    if(!issueLinkHref){
+        issueLinkHref=window.location.href;
+    }
+    if(extOptions.otherRepos.length > 1){
+        return createCloneToButtons(issueLinkHref);
+    }
     const cloneButton = document.createElement('button');
     cloneButton.id = 'ext_clone';
     cloneButton.className = 'Button Button--small Button--secondary flex-md-order-2';
     cloneButton.textContent = 'Clone';
-    cloneButton.onclick = () => cloneIssue(false);
+    cloneButton.onclick = () => cloneIssue(cloneButton.dataset.extIssue,currentRepo);
     return cloneButton;
+};
+
+// Create "Clone to" button element
+const createCloneToButtons = (issueLinkHref) => {
+    const buttonsList = document.createElement('div');
+    buttonsList.id = 'ext_clone';
+    buttonsList.classList.add('buttonsList');
+    const buttonsListContainer = document.createElement('div');
+    buttonsListContainer.classList.add('buttonsContainer');
+    const trigger = document.createElement('div');
+    trigger.textContent = "Clone to";
+    trigger.className = 'cloneToTrigger ext_trigger';
+    trigger.onclick = (e) => e.target.classList.toggle('open');
+    buttonsList.append(trigger, buttonsListContainer);
+    const cloneButton = document.createElement('div');
+    cloneButton.className = 'cloneButton';
+    cloneButton.textContent = 'This repo';
+    cloneButton.dataset.extIssue = issueLinkHref;
+    cloneButton.onclick = () => cloneIssue(cloneButton.dataset.extIssue,currentRepo);
+    buttonsListContainer.appendChild(cloneButton);
+    extOptions.otherRepos.forEach(repo => {
+        if(repo!=currentRepo){
+        const cloneButton = document.createElement('div');
+        cloneButton.className = 'cloneButton';
+        cloneButton.textContent = repo;
+        cloneButton.dataset.extIssue = issueLinkHref;
+        cloneButton.onclick = () => cloneIssue(cloneButton.dataset.extIssue,repo);
+        buttonsListContainer.appendChild(cloneButton);
+        }
+    });
+    return buttonsList;
 };
 
 // Add "New from template" button to issues list
@@ -108,7 +160,7 @@ const createTemplatesList = (templateData) => {
 const createTemplateTrigger = () => {
     const trigger = document.createElement('div');
     trigger.textContent = "New from template";
-    trigger.classList.add('templatesTrigger');
+    trigger.className='templatesTrigger ext_trigger';
     trigger.onclick = (e) => e.target.classList.toggle('open');
     return trigger;
 };
@@ -117,7 +169,7 @@ const createTemplateTrigger = () => {
 const createTemplateLink = (template) => {
     const templateLink = document.createElement('a');
     templateLink.classList.add('newWithTemplate');
-    templateLink.href = getTemplateURL(template);
+    templateLink.href = getTemplateURL(template,currentRepo);
     templateLink.target = '_blank';
     templateLink.textContent = template.title;
     return templateLink;
@@ -129,9 +181,10 @@ const addCloneButtonToIssueLinks = () => {
         const issueLinks = document.querySelectorAll(`.js-issue-row a[href^="${issueURLPattern}"][id^="issue_"]:not(.ext_processed)`);
         issueLinks.forEach(issueLink => {
             issueLink.classList.add('ext_processed');
-            const cloneButton = createCloneButton();
-            cloneButton.dataset.extIssue = issueLink.href;
-            cloneButton.onclick = () => cloneIssue(cloneButton.dataset.extIssue);
+            const cloneButton = createCloneButton(issueLink.href);
+            cloneButton.removeAttribute('id');
+            //cloneButton.dataset.extIssue = issueLink.href;
+            //cloneButton.onclick = () => cloneIssue(cloneButton.dataset.extIssue,currentRepo);
             issueLink.parentNode.after(cloneButton);
         });
     }
@@ -147,9 +200,6 @@ const getNewIssueURL = () => {
     return '';
 };
 
-// Base to build creation urls
-const createURL = getNewIssueURL()+'?';
-
 // Pattern to detect issues links
 const getIssueURLPattern = () => {
     const url = new URL(window.location.href);
@@ -159,11 +209,12 @@ const getIssueURLPattern = () => {
     }
     return '';
 };
+
 // Pattern to detect issues links
 const issueURLPattern = getIssueURLPattern();
 
 // Clone issue based on provided link or current page URL
-const cloneIssue = (issueLink) => {
+const cloneIssue = (issueLink,repo) => {
     if (!issueLink) issueLink = window.location.href;
     const url = new URL(issueLink);
     const issueAPIURL = (url.origin !== 'https://github.com')
@@ -185,15 +236,15 @@ const cloneIssue = (issueLink) => {
             labels: (issueData.labels && issueData.labels.map(label => label.name)) || [],
             assignees: (issueData.assignees && issueData.assignees.map(assignee => assignee.login)) || [],
         };
-        const newIssueURL = getTemplateURL(clonedIssue);
+        const newIssueURL = getTemplateURL(clonedIssue,repo);
         window.open(newIssueURL, '_blank');
     })
     .catch(console.error);
 };
 
 // Get template URL for creating a new issue
-const getTemplateURL = (template) => {
-    let url = createURL + `title=${encodeURIComponent(template.title)}`;
+const getTemplateURL = (template,repo) => {
+    let url = '/'+repo+'/issues/new?' + `title=${encodeURIComponent(template.title)}`;
     if (template.body) url += `&body=${encodeURIComponent(template.body)}`;
     if (template.milestone) url += `&milestone=${encodeURIComponent(template.milestone)}`;
     if (template.labels && template.labels.length) url += `&labels=${encodeURIComponent(template.labels.join(','))}`;
